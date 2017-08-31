b10b8cc1f4e97b06cf2c62df491d380cdbf80db9
Jeff Cleverley <jeff@jeffcleverley.com>
2017-08-31 15:58:33 +0800

    initial commit

---
+++ bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Better Asset Versioning LAB
+ *
+ * @package     KnowTheCode\BetterAssetVersioning
+ * @author      hellofromTonya
+ * @license     GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: Better Asset Versioning LAB
+ * Plugin URI:  https://github.com/KnowTheCode/BetterAssetVersioning
+ * Description: Improve asset version control by embedding the version number into the URL instead of as an optional query parameter.
+ * Version:     1.0.0
+ * Author:      hellofromTonya
+ * Author URI:  https://KnowTheCode.io
+ * Text Domain: better-asset-versioning
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+namespace KnowTheCode\BetterAssetVersioning;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'Cheatin&#8217; uh?' );
+}
+
